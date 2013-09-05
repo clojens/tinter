ea71a5efde0f1a8a6e4585c2a2d3092776e27a00
Rob Jentzema <rob.jentzema@googlemail.com>
2013-09-05 04:08:57 +0200

    update clojure version to 1.5.1

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject tinter "0.1.1-SNAPSHOT"
+(defproject tinter "0.1.2"
   :description "Tinter is a color conversion and manipulation library for Clojure."
   :url "http://github.com/andypayne/tinter"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
-
