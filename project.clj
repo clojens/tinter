76175fa490a4915de16fa8e30ba54a9a42b07e59
Rob Jentzema <rob.jentzema@googlemail.com>
2013-09-05 04:07:12 +0200

    bump math.numeric-tower to 0.0.2

--- project.clj
+++ project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]])
+                 [org.clojure/math.numeric-tower "0.0.2"]])
 
